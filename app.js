6bb503eab94c11dcac3d14b190aa63dc46484f7a
bahaztariq <bahaztariq@gmail.com>
2025-11-18 11:48:08 +0100

    adding modal for show infos

---
+++ app.js
@@ -0,0 +1,24 @@
+const modals = document.querySelectorAll('.modal');
+const addBtn =document.getElementById('add-worker-btn');
+const closemodal =document.querySelectorAll('.close-Modal-btn');
+const addForum  = document.getElementById('addForum');
+const detailsmodal = document.getElementById('details-model');
+
+
+
+modals.forEach((mdl)=>{
+    mdl.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('modal')){
+            mdl.classList.add('hidden');
+        }
+    })
+})
+
+addBtn.addEventListener('click',()=>{
+        addForum.classList.remove('hidden');
+})
+closemodal.forEach((closemdl)=>{
+    closemdl.addEventListener('click',(e)=>{
+            addForum.classList.add('hidden');
+    })
+})
